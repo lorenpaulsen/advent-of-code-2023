e40eaca0b223fa48db0d3ed32e4a8ad9609dfba5
Loren Paulsen <lorenpaulsen@github.com>
2023-12-03 13:32:26 -0800

    add day 2

---
+++ src/Day2/Day2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    public class Day2
+    {
+        public static int RunPart1(string input)
+        {
+            var totals = new Dictionary<string, int>()
+            {
+                ["red"] = 12,
+                ["green"] = 13,
+                ["blue"] = 14
+            };
+
+            return input
+             .Split(Environment.NewLine)
+             .Select(x => x.Split(":"))
+             .Select(x => new
+             {
+                 GameNumber = int.Parse(x[0].Split(" ")[1]),
+                 Samples = x[1]
+                    .Split(";")
+                    .Select(sample => sample
+                        .Split(",")
+                        .Select(x => x.Trim().Split(" "))
+                        .Select(x => new
+                        {
+                            Color = x[1],
+                            Count = int.Parse(x[0])
+                        })
+                    )
+             })
+             .Where(x => x.Samples
+                    .All(sample => sample
+                        .All(value => totals[value.Color] >= value.Count)))
+            .Sum(x => x.GameNumber);
+        }
+
+        public static int RunPart2(string input)
+        {
+            return input
+             .Split(Environment.NewLine)
+             .Select(x => x.Split(":")[1])
+             .Select(x =>
+                 x.Split(";")
+                .Select(sample => sample
+                    .Split(",")
+                    .Select(x => x.Trim().Split(" "))
+                    .Select(x => new
+                    {
+                        Color = x[1],
+                        Count = int.Parse(x[0])
+                    })
+                )
+             )
+             .Select(x => x
+                .SelectMany(y => y)
+                .GroupBy(y => y.Color)
+                .Select(y => new
+                {
+                    Color = y.Key,
+                    MinRequired = y.Max(z => z.Count)
+                })
+             )
+             .Sum(x => x
+                .Aggregate(1, (acc, x) => acc *= x.MinRequired));
+        }
+    }
+}
--- tests/Day1/Day1Tests.cs
+++ tests/Day1/Day1Tests.cs
@@ -22,5 +22,23 @@ namespace AdventOfCode2023.Tests
             var actual = await Day1.Run(input);
             Assert.AreEqual(54019, actual);
         }
+
+        [TestMethod()]
+        public void RunTestAsync2()
+        {
+            var input = File.ReadAllText("Day2/Input.txt");
+
+            var actual = Day2.RunPart1(input);
+            Assert.AreEqual(2406, actual);
+        }
+
+        [TestMethod()]
+        public void RunTestAsync3()
+        {
+            var input = File.ReadAllText("Day2/Input.txt");
+
+            var actual = Day2.RunPart2(input);
+            Assert.AreEqual(78375, actual);
+        }
     }
 }
